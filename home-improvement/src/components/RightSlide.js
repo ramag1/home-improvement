50e0aa730b2434ca9b70be86b89d42e5a2858416
rmagley <rachelmagley@gmail.com>
2022-04-21 20:06:12 -0700

    Add app styling

--- home-improvement/src/components/RightSlide.js
+++ home-improvement/src/components/RightSlide.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './RightSlide.css';
+import { MdOutlineArrowForwardIos } from 'react-icons/md';
 
 function RightSlide(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+	return (
+		<div className='app__rightslide'>
+			<div className='rightslide__icon'>
+				<MdOutlineArrowForwardIos size='10em' color='lightgray' />
+			</div>
+		</div>
+	);
 }
 
 export default RightSlide;
