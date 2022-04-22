fd07372ba36dc4d605e9283fd056c185e183fad4
rmagley <rachelmagley@gmail.com>
2022-04-21 20:07:00 -0700

    Add header component and styling

--- home-improvement/src/components/Header.js
+++ home-improvement/src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Header.css';
 
 function Header(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+	return (
+		<div className='app__header'>
+			<h1 className='header__h1'>Home Improvement</h1>
+		</div>
+	);
 }
 
 export default Header;
