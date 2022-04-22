44747d440d663d8d81d30f81a452dea58ee7ea6c
rmagley <rachelmagley@gmail.com>
2022-04-21 20:07:48 -0700

    Add Filter bar detail

--- home-improvement/src/components/FilterBar.js
+++ home-improvement/src/components/FilterBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { MdFilterAlt } from 'react-icons/md';
 
 function FilterBar(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+	return (
+		<div className='app__filterbar'>
+			<p>
+				Filter by <MdFilterAlt />
+			</p>
+		</div>
+	);
 }
 
 export default FilterBar;
