78a5fb30e6819463c592c74a95dfb20eee147334
rmagley <rachelmagley@gmail.com>
2022-04-21 20:15:37 -0700

    Add Images detail

--- home-improvement/src/components/Images.js
+++ home-improvement/src/components/Images.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './Images.css';
 
 function Images(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+	return (
+		<div className='app__images'>
+			<div className='images__container'>
+				<div className='images__img1'>img 1</div>
+				<div className='images__img2'> 2</div>
+				<div className='images__img3'>3</div>
+				<div className='images__img4'>4</div>
+				<div className='images__img5'>5</div>
+				<div className='images__img6'>6</div>
+			</div>
+		</div>
+	);
 }
 
 export default Images;
